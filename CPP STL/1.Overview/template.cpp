e2e7c8444d6ecf4af92547490eb3634ada9b1332
Ankit <a53gau@gmail.com>
2021-09-02 23:08:42 +0530

    mod

---
+++ CPP STL/1.Overview/template.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+template <typename T>
+T myMax(T x, T y)
+{
+    return (x > y) ? x : y;
+}
+int main()
+{
+    cout << myMax<int>(3, 7) << endl;
+    cout << myMax<char>('c', 'g') << endl;
+    cout << myMax<double>(20.2, 10.5) << endl;
+    return 0;
+}
